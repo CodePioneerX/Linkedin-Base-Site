2bcc66c9862c9ce0055c17daae116fe8dcf08102
haris2055 <78054284+haris2055@users.noreply.github.com>
2023-01-25 20:59:51 -0500

    Default Blank Templates
    These pages are for current and future development

---
+++ frontend/src/screens/ViewProfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ViewProfile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ViewProfile</h1>
+      </div>
+    )
+  }
+}
