f3fb8a2ea562804657d6de0ed1397557a01caf50
haris2055 <78054284+haris2055@users.noreply.github.com>
2023-01-25 20:54:00 -0500

    Header V1.0
    Initial Header design, further versions might include icons or spacing

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../logo.jpg";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MyNetwork from "../screens/MyNetwork";
+import Jobs from "../screens/Job";
+import Messaging from "../screens/Messaging";
+import Notification from "../screens/Notification";
+import Settings from "../screens/Settings";
+import ViewProfile from "../screens/ViewProfile";
+import Home from "../screens/Home";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar
+            collapseOnSelect
+            expand="lg"
+            style={{ backgroundColor: "white" }}
+            className="navigation"
+          >
+            <Container>
+              <Navbar.Brand as={Link} to={"/"}>
+                <img
+                  src={logo}
+                  width="170"
+                  height="35"
+                  className="d-inline-block align-top"
+                  alt="Logo"
+                />
+              </Navbar.Brand>
+
+              <Form className="d-flex"> 
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+              </Form>
+
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse
+                id="responsive-navbar-nav"
+                className="justify-content-end"
+              >
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to={"/"} style={{ paddingRight: "50px" }}>
+                    Home
+                  </Nav.Link>
+                  <Nav.Link
+                    as={Link}
+                    to={"/myNetwork"}
+                    style={{ paddingRight: "50px" }}
+                  >
+                    My Network
+                  </Nav.Link>
+                  <Nav.Link
+                    as={Link}
+                    to={"/jobs"}
+                    style={{ paddingRight: "50px" }}
+                  >
+                    Jobs
+                  </Nav.Link>
+                  <Nav.Link
+                    as={Link}
+                    to={"/messaging"}
+                    style={{ paddingRight: "50px" }}
+                  >
+                    Messaging
+                  </Nav.Link>
+                  <Nav.Link
+                    as={Link}
+                    to={"/notification"}
+                    style={{ paddingRight: "50px" }}
+                  >
+                    Notification
+                  </Nav.Link>
+                  <NavDropdown title="Profile" id="collasible-nav-dropdown">
+                    <NavDropdown.Item as={Link} to={"/viewProfile"}>
+                      View Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to={"/settings"}>
+                      Settings
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/myNetwork" element={<MyNetwork />} />
+
+            <Route path="/jobs" element={<Jobs />} />
+
+            <Route path="/messaging" element={<Messaging />} />
+
+            <Route path="/notification" element={<Notification />} />
+
+            <Route path="/viewProfile" element={<ViewProfile />} />
+
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
+}
