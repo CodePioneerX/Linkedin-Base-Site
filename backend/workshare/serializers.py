7e2ad6a5d139318724e33428be1cbcb7683a2931
Khaled <khaled.saber8@gmail.com>
2023-01-28 23:45:00 -0500

    changing singing up

--- backend/workshare/serializers.py
+++ backend/workshare/serializers.py
@@ -15,7 +15,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user( validated_data['username'], validated_data['email'])
-        
         user.set_password(validated_data['password'])
         user.save()
         return user
