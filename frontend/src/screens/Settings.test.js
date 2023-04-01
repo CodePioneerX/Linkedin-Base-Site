187970c95ce79c3d63409f41e2f0632c9625f57d
Victoria Castelli <vcastelli@bell.net>
2023-04-01 17:15:09 -0400

    Wrote some new tests to test to see if the labels, buttons, headings and placeholders render properly in settings.test.js

--- frontend/src/screens/Settings.test.js
+++ frontend/src/screens/Settings.test.js
@@ -1,14 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Settings from './Settings';
 import  {Provider} from 'react-redux';
 import store from '../store';
 
 
-test('Loads webpage', () => {
+test('Settings should have the correct headings, labels and placeholders', () => {
   render(<Provider store={store}><Settings/></Provider>);
 
-  const title = screen.queryByText('Settings');
-  const subtitle = screen.queryByText('Reset Password');
+  const heading = screen.queryByText('Settings');
+  const subheading = screen.queryByText('Reset Password');
   const oldPasswordLabel = screen.queryByLabelText('Old Password');
   const newPasswordLabel = screen.queryByLabelText('New Password');
   const confirmNewPasswordLabel = screen.queryByLabelText('Confirm New Password');
@@ -16,8 +16,8 @@ test('Loads webpage', () => {
   const newPasswordBox = screen.queryByPlaceholderText('Enter new password');
   const confirmNewPasswordBox = screen.queryByPlaceholderText('Renter your new password');
 
-  expect(title).toBeInTheDocument();
-  expect(subtitle).toBeInTheDocument();
+  expect(heading).toBeInTheDocument();
+  expect(subheading).toBeInTheDocument();
   expect(oldPasswordLabel).toBeInTheDocument();
   expect(newPasswordLabel).toBeInTheDocument();
   expect(confirmNewPasswordLabel).toBeInTheDocument();
@@ -26,5 +26,29 @@ test('Loads webpage', () => {
   expect(confirmNewPasswordBox).toBeInTheDocument();
 });
 
+test('renders "Submit New Password" button', () => {
+  render(<Provider store={store}><Settings/></Provider>);
+  const button = screen.getByRole('button', { name: /submit new password/i });
+  expect(button).toBeInTheDocument();
+});
 
+test('calls submitHandler function on form submission', () => {
+  const mockSubmitHandler = jest.fn();
+  render(<Provider store={store} onSubmit={mockSubmitHandler}><Settings/></Provider>);
+  fireEvent.submit(screen.getByRole('button', { name: /submit new password/i }));
+  expect(mockSubmitHandler).toHaveBeenCalled();
+});
 
+test('dispatches changePassword action with correct arguments', async () => {
+  const dispatch = jest.fn();
+  useDispatch.mockReturnValue(dispatch);
+  useSelector.mockReturnValue({ userInfo: { id: 1 } });
+  render(<Provider store={store}><Settings/></Provider>);
+  fireEvent.change(screen.getByLabelText('Old Password'), { target: { value: 'oldPassword' } });
+  fireEvent.change(screen.getByLabelText('New Password'), { target: { value: 'newPassword' } });
+  fireEvent.change(screen.getByLabelText('Confirm New Password'), { target: { value: 'newPassword' } });
+  fireEvent.submit(screen.getByRole('button', { name: /submit new password/i }));
+  await waitFor(() => {
+    expect(dispatch).toHaveBeenCalledWith(changePassword(1, 'oldPassword', 'newPassword'));
+  });
+});
