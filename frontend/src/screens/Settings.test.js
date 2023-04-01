c2df10894cea3e4b9065968ca56d5a90b7bc4aab
Victoria Castelli <vcastelli@bell.net>
2023-04-01 16:45:31 -0400

    Removed arbitrary node modules folder

--- frontend/src/screens/Settings.test.js
+++ frontend/src/screens/Settings.test.js
@@ -1,14 +1,29 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import user from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import Settings from './Settings';
+import  {Provider} from 'react-redux';
+import store from '../store';
 
 
+test('Loads webpage', () => {
+  render(<Provider store={store}><Settings/></Provider>);
 
-test('Loads webpage', async () => {
-  render(<Settings />);
+  const title = screen.queryByText('Settings');
+  const subtitle = screen.queryByText('Reset Password');
+  const oldPasswordLabel = screen.queryByLabelText('Old Password');
+  const newPasswordLabel = screen.queryByLabelText('New Password');
+  const confirmNewPasswordLabel = screen.queryByLabelText('Confirm New Password');
+  const oldPasswordBox = screen.queryByPlaceholderText('Enter current password');
+  const newPasswordBox = screen.queryByPlaceholderText('Enter new password');
+  const confirmNewPasswordBox = screen.queryByPlaceholderText('Renter your new password');
 
-  const title = await screen.queryByText('Settings'); //screen.QuerryBt('Settings');
   expect(title).toBeInTheDocument();
+  expect(subtitle).toBeInTheDocument();
+  expect(oldPasswordLabel).toBeInTheDocument();
+  expect(newPasswordLabel).toBeInTheDocument();
+  expect(confirmNewPasswordLabel).toBeInTheDocument();
+  expect(oldPasswordBox).toBeInTheDocument();
+  expect(newPasswordBox).toBeInTheDocument();
+  expect(confirmNewPasswordBox).toBeInTheDocument();
 });
 
 
