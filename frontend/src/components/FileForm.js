f5614a417c074645741163de8803c06dd74f3a45
Jiayi Chen <cjy0427615@gmail.com>
2023-04-13 02:54:35 -0400

    [US-44] [US-45] feat: Add clear button #140 #139
    Add clear button in the documents upload form. Display a message when the user doesn't enter upload any files but still click save. The user is allowed to only reupload one of the two files.

--- frontend/src/components/FileForm.js
+++ frontend/src/components/FileForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import {  Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
 import '../Assets/css/App.css';
@@ -11,17 +11,38 @@ const FileForm = ({ fileForm,setFileForm }) => {
 //Get the user information from the store
 const userLogin = useSelector((state) => state.userLogin);
 const { userInfo } = userLogin;
+const resumeRef = useRef();
+const coverLetterRef = useRef();
+
 
 //Holds and sets the value
-const [resume, setResume] = useState(null);    
-const [coverLetter, setCoverLetter]= useState(null); 
+const [resume, setResume] = useState();    
+const [coverLetter, setCoverLetter]= useState(); 
+
 
 //Handle the submission, send the file to the backend
 const FileUpload = (e)=>{
     e.preventDefault();
+    if(!resume){
+      if(!coverLetter){
+      alert("The file inputs cannot be all empty!");
+      return;
+      }
+    }
     console.log(resume);
     console.log(coverLetter);
-    setFileForm(prev => !prev)
+    // setFileForm(prev => !prev)
+}
+
+const FileClear = (e)=>{
+  e.preventDefault();
+  resumeRef.current.value = "";
+  coverLetterRef.current.value = "";
+  setResume();
+  setCoverLetter();
+  console.log(resume);
+  console.log(coverLetter);
+
 }
 
 return (
@@ -34,16 +55,17 @@ return (
           <FormGroup className='FileInputContent'>
             <h2 className='FileInputText'>Resume</h2>
             <Label className='Resume' for="Resume"/>
-            <input type='file' name="Resume" id="Resume" 
+            <input type='file' name="Resume" id="Resume" ref={resumeRef}
             onChange={(e)=> {setResume(e.target.files[0])}}/>
             </FormGroup>
           <FormGroup className='FileInputContent'>
             <h2 className='FileInputText'>Cover Letter</h2>
             <Label className='CoverLetter' for="CoverLetter"/>
-            <input type='file' name="CoverLetter" id="CoverLetter" 
+            <input type='file' name="CoverLetter" id="CoverLetter" ref={coverLetterRef}
             onChange={(e)=> {setCoverLetter(e.target.files[0])}}/>
-            </FormGroup>
-          <button className='file-form-button' onClick={FileUpload}>Save</button>
+          </FormGroup>
+            <button className='file-form-button' onClick={FileUpload}>Save</button>
+            <button className='file-form-clear-button' onClick={FileClear}>Clear</button>
           </Form>
         </div>
         <MdClose className='close-file-form-button'
