dfcb0e9cce86c76ba4a2f4e3e14e4721f61055ec
z-bruce <87652520+z-bruce@users.noreply.github.com>
2023-04-22 22:18:39 -0400

    [testing] add unit tests for post comments & likes
    This commit adds unit tests for post comments and likes.

--- backend/workshare/tests/test_posts.py
+++ backend/workshare/tests/test_posts.py
@@ -33,3 +33,49 @@ class PostListingCreateViewTestCase(APITestCase):
         response = self.client.post(url, data=self.data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class CommentTestCase(APITestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='testuser', email='testuser@example.com', password='testpass')
+        self.user2 = User.objects.create_user(username='testuser2', email='testuser2@example.com', password='testpass')
+
+        self.post1 = Post.objects.create(author=self.user1, title='test post 1 title', content='content of test post 1')
+
+        self.comment_1_data = {'user_id':self.user1.id, 'content': 'content of comment 1'}
+        self.comment_2_data = {'user_id':self.user1.id, 'content': ''}
+
+    def test_create_comment(self):
+        response = self.client.post(reverse('create_comment', args=[self.post1.id]), data=self.comment_1_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_empty_comment(self):
+        response = self.client.post(reverse('create_comment', args=[self.post1.id]), data=self.comment_2_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json()['error'], 'Your comment cannot be empty!')
+
+class LikeTestCase(APITestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='testuser', email='testuser@example.com', password='testpass')
+        self.user2 = User.objects.create_user(username='testuser2', email='testuser2@example.com', password='testpass')
+
+        self.post1 = Post.objects.create(author=self.user1, title='test post 1 title', content='content of test post 1')
+
+        self.like_1_data = {'user_id': self.user2.id}
+
+    def test_create_like(self):
+        response = self.client.post(reverse('like_post', args=[self.post1.id]), data=self.like_1_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_like_already_liked(self):
+        response = self.client.post(reverse('like_post', args=[self.post1.id]), data=self.like_1_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['liked'], True)
+        
+        response_2 = self.client.post(reverse('like_post', args=[self.post1.id]), data=self.like_1_data)
+
+        self.assertEqual(response_2.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_2.json()['liked'], False)
+        
+    def test_create_like_fail(self):
+        response = self.client.post(reverse('like_post', args=[999]), data=self.like_1_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
